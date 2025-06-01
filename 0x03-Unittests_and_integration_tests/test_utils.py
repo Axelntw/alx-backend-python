ba4354a87f93fbe4fd1a65c1da72df97e98bd393
Axelntw <ntwaliaxel@gmail.com>
2025-06-01 12:51:10 +0200

    Task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@
 import unittest
 from unittest.mock import patch
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -34,6 +34,29 @@ class TestGetJson(unittest.TestCase):
             
             mock_get.assert_called_once_with(test_url)
             self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """Tests for memoize decorator"""
+
+    def test_memoize(self):
+        """Test that when calling a_property twice, a_method is only called once"""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        
+        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
+            test_class = TestClass()
+            first_call = test_class.a_property
+            second_call = test_class.a_property
+
+            self.assertEqual(first_call, 42)
+            self.assertEqual(second_call, 42)
+            mock_method.assert_called_once()
 
 
 if __name__ == '__main__':
