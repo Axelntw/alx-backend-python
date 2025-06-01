14823237fbdd0e4b2d7540e7bc604fe7c02f44b1
Axelntw <ntwaliaxel@gmail.com>
2025-06-01 15:05:56 +0200

    Fixes

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -2,10 +2,18 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
+# Initialize DefaultRouter
 router = DefaultRouter()
-router.register(r'conversations', ConversationViewSet)
-router.register(r'conversations/(?P<conversation_pk>[^/.]+)/messages', MessageViewSet)
 
+# Register viewsets with the router
+router.register(r'conversations', ConversationViewSet, basename='conversation')
+router.register(
+    r'conversations/(?P<conversation_pk>[^/.]+)/messages',
+    MessageViewSet,
+    basename='conversation-messages'
+)
+
+# Wire up our API using automatic URL routing
 urlpatterns = [
     path('', include(router.urls)),
 ]
